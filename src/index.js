2908363e84115d17bd92eb9239d7d7dead054532
mr.xie <393197906@qq.com>
2019-01-25 18:21:03 +0800

    #add 升级vea 到 1.1.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { vue as Vue } from 'vea'
+import { vue as Vue, extendVue } from 'vea'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 import service from './service.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+Vue.use(extendVue)
 Vue.prototype.$service = service(Vue)
 Vue.use(ElementUI)
 /* eslint-disable no-new */
