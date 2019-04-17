84905f4ceb3660339b9c2ca57ea442402168d703
mr.xie <393197906@qq.com>
2019-04-17 10:44:35 +0800

    #add  vue$

--- vea.config.js
+++ vea.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 module.exports = {
   alias: {
-    '@': path.resolve(__dirname, './src/')
+    '@': path.resolve(__dirname, './src/'),
+    'vue$': 'vue/dist/vue.esm.js'
   },
   eslintLoaderOptions: {
     emitError: true,
